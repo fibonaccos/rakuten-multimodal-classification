48d0f7918716200f54386fa61b159fad11e4f85c
fibonaccos <bryan.khanmahmood@gmail.com>
2025-07-10 21:45:37 +0200

    Ajout structures des pipelines

--- src/preprocessing/main_pipeline.py
+++ src/preprocessing/main_pipeline.py
@@ -13,6 +13,18 @@ def pipe() -> None:
 def text_pipe() -> None:
     """
     The textual datasets pipeline.
+    1. Copier les datasets -> ok \\
+    -> Début Pipeline sklearn
+    2. CharacterCleaner sur xtrain et xtest -> ok
+    3. Vectorisation sur xtrain et xtest -> ok
+    4. Restructurer les datasets xtrain et xtest (éclatement des embeddings en colonnes)
+    5. Remplissage des valeurs manquantes
+    6. Re-sampling des classes \\
+    -> Fin Pipeline sklearn
+    7. Renommage des classes -> ok
+    8. Sauvegarde -> ok
+
+    Rajout de process facilitée par la structure adaptative.
     """
     ...
 
@@ -20,5 +32,11 @@ def text_pipe() -> None:
 def image_pipe() -> None:
     """
     The image datasets pipeline.
+    1. Copier les datasets -> ok
+    2. Pooling -> ok
+    3. Réduction de canaux -> ok
+    4. Sauvegarde -> ok
+
+    Rajout de process facilitée par la structure adaptative.
     """
     ...
