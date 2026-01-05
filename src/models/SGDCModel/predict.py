97e5fd43f2c02527fbfc3c874121b442b0afb87d
TRINCAL <99663281+SteeveGitHub@users.noreply.github.com>
2026-01-05 22:58:22 +0100

    modifs avant prez

--- src/models/SGDCModel/predict.py
+++ src/models/SGDCModel/predict.py
@@ -56,6 +56,7 @@ def predict(logger, input_features=None):
         'prediction': predictions,
         'prediction_encoded': predictions_encoded
     }, index=input_features.index)
+    predictions_df.index.name = 'id'
     
     predictions_df.to_csv(predict_config['output']['predictions_path'])
     
@@ -65,6 +66,7 @@ def predict(logger, input_features=None):
             columns=label_encoder.classes_,
             index=input_features.index
         )
+        proba_df.index.name = 'id'
         proba_df.to_csv(predict_config['output']['probabilities_path'])
         logger.info("Probabilities saved")
     
