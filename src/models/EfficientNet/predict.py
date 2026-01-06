47ab37b4787214245d0e7fa0f374d027a1f18f34
Rizlene Banat <grizzly@air-de-rizlene.home>
2026-01-06 20:07:21 +0100

    predict v3 : add some comments

--- src/models/EfficientNet/predict.py
+++ src/models/EfficientNet/predict.py
@@ -1,50 +1,33 @@
-import pandas as pd
 import numpy as np
 import os
 import torch
 import torch.nn as nn
-import torch.optim as optim
 from torchvision import transforms, models
 from torch.utils.data import DataLoader, Dataset
-import torch.nn.functional as F
-from PIL import Image, ImageDraw, ImageFont
-import time
+from PIL import Image
 import random
 import matplotlib.pyplot as plt
 
-# Fixer les graines aléatoires
-seed = 42  # Vous pouvez choisir n'importe quel entier ici
+# Reproductibility
+seed = 42  
 torch.manual_seed(seed)
 np.random.seed(seed)
 random.seed(seed)
 
 # Constants
-IMAGE_SIZE = (224, 224)  # Dimension des images
+IMAGE_SIZE = (224, 224) 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# Directory
 ANCR_DIR = "./"
 IMG_TEST_DIR = ANCR_DIR+'data/images/image_test/'
 
-# Affichage des classes après encodage
-encoded_classes = [18, 19, 16, 5, 8, 
-                   25, 11, 4, 23, 9, 
-                   13, 14, 22, 7, 21, 
-                   0, 24, 3, 6, 2, 
-                   15, 26, 10, 12, 1, 
-                   17, 20]
-
-# Affichage des classes d'origine
-original_classes = [2280, 2403, 2060, 1160, 1281, 
-                    2705, 1302, 1140, 2583, 1300, 
-                    1560, 1920, 2582, 1280, 2522, 
-                    10, 2585, 60, 1180, 50, 
-                    1940, 2905, 1301, 1320, 40, 
-                    2220, 2462]
-
-# Créer le mapping
+# Mapping
+encoded_classes = [18, 19, 16, 5, 8, 25, 11, 4, 23, 9, 13, 14, 22, 7, 21, 0, 24, 3, 6, 2, 15, 26, 10, 12, 1, 17, 20]
+original_classes = [2280, 2403, 2060, 1160, 1281, 2705, 1302, 1140, 2583, 1300, 1560, 1920, 2582, 1280, 2522, 10, 2585, 60, 1180, 50, 1940, 2905, 1301, 1320, 40, 2220, 2462]
 label_mapping = dict(zip(encoded_classes, original_classes))
 
-# Transformations des données
+# Data transformation
 transform_test = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
@@ -76,7 +59,7 @@ class CustomImageDataset(Dataset):
 
         return image, self.image_filenames[idx]
 
-# Charger EfficientNet
+# Class EfficientNet
 class EfficientNetModel(nn.Module):
     def __init__(self, num_classes):
         super(EfficientNetModel, self).__init__()
@@ -86,14 +69,13 @@ class EfficientNetModel(nn.Module):
     def forward(self, x):
         return self.model(x)
 
-def predict_classe(img_dir) :
+# Prediction with EfficientNet
+def predict_efficientNet(img_dir) :
   # Charger les datasets
-  test_dataset = CustomImageDataset(img_dir,
-                                    transform=transform_test)
-
+  test_dataset = CustomImageDataset(img_dir, transform=transform_test)
   test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-  # Validation du modèle
+  # Chargement du modèle
   model = EfficientNetModel(num_classes=27)
   model.to(DEVICE)
   try:
@@ -102,20 +84,20 @@ def predict_classe(img_dir) :
       pass
 
   model.load_state_dict(torch.load(f'{ANCR_DIR}models/EfficientNet/model_efficientNet.pth', map_location=DEVICE))
+  
+  # Prédiction du modèle
   model.eval()  # Passer en mode évaluation
   with torch.no_grad():
     all_predictions = []
     all_probabilities = []
 
     for images, filename in test_loader:
-
+        # Récupération des images à tester
         images = images.to(DEVICE)
         outputs = model(images)
 
-        # Obtenir les probabilités avec softmax
+        # Obtenir les probabilités et les classes prédites
         probabilities = torch.softmax(outputs, dim=1)
-
-        # Obtenir les classes prédites
         _, predicted = torch.max(probabilities.data, 1)
 
         # Rassembler les prédictions et les probabilités
@@ -125,14 +107,15 @@ def predict_classe(img_dir) :
         idx = 0
         # Afficher les images avec les classes et probabilités
         for i in range(images.size(0)):
-            img_tensor = images[i].cpu().detach()  # Obtention du tenseur d'image
-            img_numpy = img_tensor.permute(1, 2, 0).numpy()  # Changer l'ordre des dimensions
+            # Récupérer l'image
+            img_tensor = images[i].cpu().detach()
+            img_numpy = img_tensor.permute(1, 2, 0).numpy()
 
             # Normaliser l'image
-            img_numpy = (img_numpy - img_numpy.min()) / (img_numpy.max() - img_numpy.min())  # Normaliser entre 0 et 1
-            img_numpy = (img_numpy * 255).astype(np.uint8)  # Convertir en entier 8 bits
+            img_numpy = (img_numpy - img_numpy.min()) / (img_numpy.max() - img_numpy.min())
+            img_numpy = (img_numpy * 255).astype(np.uint8)
 
-            # Ajouter le texte directement sur le tenseur ou calculer la position pour l'affichage
+            # Récupération de la prédiction de l'image
             predicted_class = all_predictions[-images.size(0) + i]
             # Mapper les valeurs
             mapped_class = label_mapping[predicted_class]
@@ -143,18 +126,19 @@ def predict_classe(img_dir) :
             plt.figure()
             plt.imshow(img_numpy)
             plt.title(text)  # Mettre le titre avec la classe et la probabilité
-            plt.axis('off')  # Ne pas afficher les axes
+            plt.axis('off') 
 
             # Sauvegarder l'image
             os.makedirs(f"{img_dir}image_predict", exist_ok=True)
-            file_name = f'predicted_{filename[idx]}'  # Nommez le fichier comme bon vous semble
+            file_name = f'predicted_{filename[idx]}'
             plt.savefig(os.path.join(f"{img_dir}image_predict", file_name), bbox_inches='tight', pad_inches=0)
-            plt.close()  # Fermer la figure
+            plt.close()
 
             idx += 1
 
     print(f"Images prédites enregistrées dans le dossier : {img_dir}")
-    
-    return(1)
 
-predictions = predict_classe(IMG_TEST_DIR)
+    return(True)
+
+predictions = predict_efficientNet(IMG_TEST_DIR)
+
