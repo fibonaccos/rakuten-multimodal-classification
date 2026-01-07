370b875465198ba99451238c535f410909d7360b
Rizlene Banat <grizzly@air-de-rizlene.home>
2026-01-07 13:38:07 +0100

    migration efficientNet

---
+++ src/models/EfficientNet/predict.py
@@ -0,0 +1,176 @@
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+from torchvision import transforms, models
+from torch.utils.data import DataLoader, Dataset
+from PIL import Image
+import random
+import matplotlib.pyplot as plt
+import time
+import sys
+import torch._dynamo
+import gc
+
+torch._dynamo.config.suppress_errors = True
+
+# Reproductibility
+seed = 42  
+torch.manual_seed(seed)
+np.random.seed(seed)
+random.seed(seed)
+
+# Constants
+IMAGE_SIZE = (224, 224) 
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Directory
+ANCR_DIR = "./"
+IMG_TEST_DIR = ANCR_DIR+'data/images/image_test/'
+
+# Mapping
+encoded_classes = [18, 19, 16, 5, 8, 25, 11, 4, 23, 9, 13, 14, 22, 7, 21, 0, 24, 3, 6, 2, 15, 26, 10, 12, 1, 17, 20]
+original_classes = [2280, 2403, 2060, 1160, 1281, 2705, 1302, 1140, 2583, 1300, 1560, 1920, 2582, 1280, 2522, 10, 2585, 60, 1180, 50, 1940, 2905, 1301, 1320, 40, 2220, 2462]
+label_mapping = dict(zip(encoded_classes, original_classes))
+
+# Data transformation
+transform_test = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+# Custom Dataset class
+class CustomImageDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+        # Lister tous les fichiers d'image dans le répertoire
+        self.image_filenames = [f for f in os.listdir(root_dir) if f.endswith('.jpg')]
+
+    def __len__(self):
+        # Retourner le nombre d'images
+        return len(self.image_filenames)
+
+    def __getitem__(self, idx):
+        # Construction du nom de l'image
+        img_name = os.path.join(self.root_dir, self.image_filenames[idx])
+
+        # Chargement de l'image
+        image = Image.open(img_name).convert('RGB')
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, self.image_filenames[idx]
+
+# Class EfficientNet
+class EfficientNetModel(nn.Module):
+    def __init__(self, num_classes):
+        super(EfficientNetModel, self).__init__()
+        self.model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)  # Charger EfficientNet avec des poids pré-entraînés
+        self.model.classifier[1] = nn.Linear(self.model.classifier[1].in_features, num_classes)  # Adapter la dernière couche
+
+    def forward(self, x):
+        return self.model(x)
+
+# Prediction with EfficientNet
+def predict_efficientNet(img_dir) :
+    t0 = time.time()
+    # Charger les datasets
+    test_dataset = CustomImageDataset(img_dir, transform=transform_test)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=0)
+
+    # Chargement du modèle
+    model = EfficientNetModel(num_classes=27)
+    model.to(DEVICE)
+    try:
+        model = torch.compile(model)
+    except Exception:
+        pass
+
+    model.load_state_dict(torch.load(f'{ANCR_DIR}models/EfficientNet/model_efficientNet.pth', map_location=DEVICE))
+  
+    # Prédiction du modèle
+    model.eval()  # Passer en mode évaluation
+    print("ok0")
+    #sys.exit(0) : ok
+    with torch.no_grad():
+        all_predictions = []
+        all_probabilities = []
+        #sys.exit(0) : ok
+
+        for images, filename in test_loader:
+            # Récupération des images à tester
+            images = images.to(DEVICE)
+            print("ok1")
+            #sys.exit(0) : ok
+
+            outputs = model(images)
+            print("ok2")
+
+            # Obtenir les probabilités et les classes prédites
+            probabilities = torch.softmax(outputs, dim=1)
+            _, predicted = torch.max(probabilities.data, 1)
+
+            # Rassembler les prédictions et les probabilités
+            all_predictions.extend(predicted.cpu().numpy())
+            all_probabilities.extend(probabilities.cpu().detach().numpy())
+            #sys.exit(0)
+
+            idx = 0
+            # Afficher les images avec les classes et probabilités
+            print("ok3")
+            for i in range(images.size(0)):
+                # Récupérer le tenseur d'image depuis le batch
+                img_tensor = images[i].cpu().detach()
+
+                # Convertir le tenseur en un format numpy
+                img_numpy = img_tensor.permute(1, 2, 0).numpy()  # Changer l'ordre des dimensions (C, H, W) à (H, W, C)
+
+                # Dé-normaliser l'image en utilisant les valeurs moyennes et les écarts-types utilisés dans la normalisation
+                mean = np.array([0.485, 0.456, 0.406])
+                std = np.array([0.229, 0.224, 0.225])
+                img_numpy = img_numpy * std + mean  # Revert la normalisation
+
+                # Clip les valeurs pour s'assurer qu'elles sont entre 0 et 1
+                img_numpy = np.clip(img_numpy, 0, 1)  
+
+                # Convertir de [0, 1] à [0, 255] et changer en entier 8 bits
+                img_numpy = (img_numpy * 255).astype(np.uint8)
+
+                # Récupération de la prédiction de l'image
+                predicted_class = all_predictions[-images.size(0) + i]
+                # Mapper les valeurs
+                mapped_class = label_mapping[predicted_class]
+                probability = all_probabilities[-images.size(0) + i][predicted_class]
+                text = f'Class: {mapped_class}, Prob: {probability:.2f}'
+
+                # Créer une nouvelle figure pour sauvegarder
+                fig, ax = plt.subplots(1, 1)  # Modifier en (1, 1) au lieu de ((1, 1))
+                ax.imshow(img_numpy)
+                ax.set_title(text)  # Mettre le titre avec la classe et la probabilité
+                ax.axis('off')  # Ne pas afficher les axes
+
+                # Sauvegarder l'image dans le répertoire approprié
+                os.makedirs(os.path.join(img_dir, 'image_predict'), exist_ok=True)  # Utiliser os.path.join pour une bonne construction de chemin
+                file_name = f'predicted_{filename[idx]}'  # Construire le nom du fichier
+                print(file_name)
+                fig.savefig(os.path.join(img_dir, 'image_predict', file_name), bbox_inches='tight', pad_inches=0)  # Sauvegarder la figure
+                plt.close(fig)  # Fermer la figure pour libérer la mémoire
+
+                idx += 1
+                print("ok4")
+            print("ok5")
+
+        print(f"Images prédites enregistrées dans le dossier : {img_dir}")
+
+    print("ok6")
+    t1 = time.time()
+    return(f"Prédiction réalisée en {(t1 - t0):.2f} secondes") #: pas ok)
+
+#predictions = predict_efficientNet(IMG_TEST_DIR)
+print("ok7")
+#sys.exit(0)
+
