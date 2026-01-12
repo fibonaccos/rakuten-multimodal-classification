bc901e6b8fe52f58fe5124fc9327efb8c5ce1c09
Rizlene Banat <grizzly@air-de-rizlene.home>
2026-01-12 12:44:34 +0100

    add streamlit conclusion

--- src/streamlit/app/pages/5_Conclusion.py
+++ src/streamlit/app/pages/5_Conclusion.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Pour encapsuler le contenu dans des fonctions et des fichiers séparés,
 # isolation des variables, etc si besoin
-from streamlit_components import conclusion
+#from streamlit_components import conclusion
 
 
 st.set_page_config(
@@ -14,5 +14,22 @@ st.set_page_config(
 
 st.title("Conclusion")
 
+col1, col2 = st.columns(2)
 
-# TODO: COMPLETER ICI RIZLENE
+with col1:
+    st.subheader("Limites")
+    st.write("##### Générales :")
+    st.write("- Mise en évidence de la complexité de l'exploration et préparation des données réelles.")
+    st.write("- Complication du développement dûe aux ressources computationnelles limités et à la collaboratioin à distance")
+   
+    st.write("##### Propres aux modèles :")
+    st.write("- Random Forest : inadapté à la haute dimensionnalité textuelle")
+    st.write("- SGDC : performance plafonnée (75% max) + ne capture pas les patterns non linéaires")
+    st.write("- EfficientNet : ressources limitées + performances passables")
+
+with col2:
+    st.subheader("Ouvertures")
+    st.write("- Amélioration nécessaire du prétraitement et de la création de variables")
+    st.write("- Optimisation des choix de modèles et des hyper-paramètres à prévoir")
+    st.write("- Multimodèles : vote à la majorité et/ou utilisation de réseaux de neuronnes denses")
+    st.write("- Workflow et pratiques de collaboration à renforcer pour une meilleure efficacité")
